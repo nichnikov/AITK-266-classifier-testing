752c28b87fda125ebe42d60a189b30a11b829980
alexeynichnikov <a.nichnikov@gmail.com>
2023-01-20 15:13:57 +0300

    data and scrips upload

--- testing.py
+++ testing.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import time
 import pandas as pd
 
 fa_url = "http://srv01.nlp.dev.msk2.sl.amedia.tech:4021/api/search"
@@ -12,10 +13,24 @@ test_texts = list(df["text"])
 classifier_search = []
 jaccard_search = []
 
-for text in test_texts[:10]:
+t = time.time()
+for text in test_texts[:100]:
     search_dict = {"pubid": 9,
                    "text": text}
     fa_r = requests.post(fa_url, json=search_dict)
     cl_r = requests.post(cl_url, json=search_dict)
-    jaccard_search.append(fa_r)
-    classifier_search.append(cl_r)
+    jaccard_search.append(fa_r.json())
+    classifier_search.append(cl_r.json())
+print("working time:", time.time() - t)
+
+classifier_search_df = pd.DataFrame(classifier_search)
+jaccard_search_df = pd.DataFrame(jaccard_search)
+
+print(classifier_search_df)
+
+classifier_search_df.to_csv(os.path.join("results", "classifier.csv"), sep="\t")
+jaccard_search_df.to_csv(os.path.join("results", "jaccard.csv"), sep="\t")
+
+print(classifier_search[:10])
+print(jaccard_search[:10])
+print(len(classifier_search), len(jaccard_search))
--- testing_async.py
+++ testing_async.py
@@ -1,6 +1,32 @@
 import os
+import aiohttp
+import asyncio
 import requests
 import pandas as pd
+
+
+async def searching(session, url, data):
+    async with session.post(url, json=data) as resp:
+        response = await resp.json()
+        return response
+
+
+async def search(send_data: {}, urls_for_searching: []):
+    results = []
+    conn = aiohttp.TCPConnector(limit=10)
+    timeout_seconds = 1
+    session_timeout = aiohttp.ClientTimeout(sock_connect=timeout_seconds, sock_read=timeout_seconds)
+    async with aiohttp.ClientSession(connector=conn, timeout=session_timeout) as session:
+        tasks = []
+        for url, sign in urls_for_searching:
+            tasks.append(asyncio.ensure_future(searching(session, url, send_data)))
+        try:
+            taking_responses = await asyncio.gather(*tasks)
+        except:
+            return {"templateId": 0, "templateText": ""}
+    for response in taking_responses:
+        results.append(response)
+
 
 fa_url = "http://srv01.nlp.dev.msk2.sl.amedia.tech:4021/api/search"
 cl_url = "http://srv01.nlp.dev.msk2.sl.amedia.tech:4022/api/search"
@@ -15,7 +41,10 @@ jaccard_search = []
 for text in test_texts[:10]:
     search_dict = {"pubid": 9,
                    "text": text}
+
+
+    '''
     fa_r = requests.post(fa_url, json=search_dict)
     cl_r = requests.post(cl_url, json=search_dict)
     jaccard_search.append(fa_r)
-    classifier_search.append(cl_r)
+    classifier_search.append(cl_r)'''
